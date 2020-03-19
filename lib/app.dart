12b38213d55d36b1cdbf5858a7fc947ada27ea24
minikin <djminikin@gmail.com>
2020-03-19 10:05:53 +0100

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Audio Player',
+      home: Center(
+        child: Text('Text'),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:audio_player_flutter/app.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
