ba9baa8850f3ebdc2c0fa60ce08dd977bdf4f4e4
Sasha Prokhorenko <djminikin@gmail.com>
2020-03-25 10:54:43 +0100

    Hotfix/update mocks (#8)
    * Add AudioFile, AudioService
    
    * Update audio_file, README
    
    * Add free_functions
    
    * Add mock_utils
    
    * Add mocks_audio_files
    
    * Add JsonStorage, update AudioFile
    
    * Minor update
    
    * Add audio file
    
    * Update iOS project, assets
    
    * Update mocks
    
    * Fix errors

--- test/utils/mocks_audio_files.dart
+++ test/utils/mocks_audio_files.dart
@@ -4,21 +4,6 @@ import 'package:uuid/uuid.dart';
 
 import 'mock_utils.dart';
 
-const _audioArtworkUrlPaths = [
-  'https://i.ibb.co/1GkYjqQ/audio-artwork-9.png',
-  'https://i.ibb.co/2MVPBjH/audio-artwork-11.png',
-  'https://i.ibb.co/5BTdbh5/audio-artwork-3.png',
-  'https://i.ibb.co/cCy726F/audio-artwork-12.png',
-  'https://i.ibb.co/cxZJFQF/audio-artwork-2.png',
-  'https://i.ibb.co/dDW1QFV/audio-artwork-4.png',
-  'https://i.ibb.co/hMpQXK3/audio-artwork-7.png',
-  'https://i.ibb.co/JQzVTv0/audio-artwork-8.png',
-  'https://i.ibb.co/tp4zgkL/audio-artwork-10.png',
-  'https://i.ibb.co/wKnrj33/audio-artwork-5.png',
-  'https://i.ibb.co/XFwzSPv/audio-artwork-1.png',
-  'https://i.ibb.co/zsFJnqx/audio-artwork-6.png',
-];
-
 const _audioFilesAuthors = [
   'Doom Deck',
   'Sleep 342',
@@ -85,8 +70,9 @@ class MockAudioFilesDataSource {
 
     final title = _audioFilesTitles[random.nextInt(_audioFilesTitles.length)];
 
+    final artworkId = randomIntInRange(1, 23);
     final artwork =
-        _audioArtworkUrlPaths[random.nextInt(_audioArtworkUrlPaths.length)];
+        'http://api-flutter-audio-player.herokuapp.com/assets/images/$artworkId.png';
 
     return AudioFile(
       (b) => b
@@ -94,7 +80,7 @@ class MockAudioFilesDataSource {
         ..artworkUrlPath = artwork
         ..audioFileUrlPath =
             'https://www.dropbox.com/s/k1gi3f1yvlneyg5/bensound-erf.mp3?dl=1'
-        ..duration = 210000
+        ..duration = 241000
         ..id = Uuid().v4()
         ..title = title,
     );
