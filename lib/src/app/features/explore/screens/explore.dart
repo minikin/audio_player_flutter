cc3649a6b8363d0f6ea5fb48e67cc09b89587840
minikin <djminikin@gmail.com>
2020-03-28 16:54:52 +0100

    Add Explore screen

---
+++ lib/src/app/features/explore/screens/explore.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Explore extends StatelessWidget {
+  const Explore({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
