c75e11841d1f3039a63136f95ed83270d0540145
minikin <djminikin@gmail.com>
2020-03-28 17:03:18 +0100

    Add ExploreItem

--- lib/src/app/features/explore/screens/widgets/explore_item.dart
+++ lib/src/app/features/explore/screens/widgets/explore_item.dart
@@ -1,8 +1,8 @@
 import 'package:audio_player_flutter/src/app/common/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
-class Explore extends StatelessWidget {
-  const Explore({Key key}) : super(key: key);
+class ExploreItem extends StatelessWidget {
+  const ExploreItem({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
