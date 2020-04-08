a90882e9781ed90741848ff46d0e3fe7cf857ad6
Sasha Prokhorenko <djminikin@gmail.com>
2020-04-08 13:52:32 +0200

    Feature/environment (#13)
    * Update README
    
    * minor update
    
    * Update pubs
    
    * Add Environment

--- lib/src/services/services.dart
+++ lib/src/services/services.dart
@@ -1,2 +1,3 @@
+export 'package:audio_player_flutter/src/services/audio/audio_service.dart';
 export 'package:audio_player_flutter/src/services/models/models.dart';
 export 'package:audio_player_flutter/src/services/repositories/api_repository.dart';
