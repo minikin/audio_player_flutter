d38b98409f1f11b479fa3c87f0f1ef542abbe2cb
Sasha Prokhorenko <djminikin@gmail.com>
2020-04-09 15:25:09 +0200

    Feature/configure app (#14)
    * Update README
    
    * minor update
    
    * Update pubs
    
    * Delete unsuded assets
    
    * Add configureApp

--- lib/src/app/app.dart
+++ lib/src/app/app.dart
@@ -1,17 +1,19 @@
 import 'package:audio_player_flutter/src/app/features/explore/screens/explore.dart';
-import 'package:audio_player_flutter/src/services/services.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
-  final _apiClient = ApiRepository();
+  const App({Key key}) : super(key: key);
+  // final _apiClient = ApiRepository();
   @override
   Widget build(BuildContext context) {
-    _fetchAllTunes();
-    return const MaterialApp(title: 'Audio Player', home: Explore());
+    return const MaterialApp(
+      title: 'Audio Player',
+      home: Explore(),
+    );
   }
 
-  void _fetchAllTunes() async {
-    final items = await _apiClient.fetchAllTunes();
-    print(items);
-  }
+  // void _fetchAllTunes() async {
+  //   final items = await _apiClient.fetchAllTunes();
+  //   print(items);
+  // }
 }
