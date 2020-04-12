d4c70b29413ce8aa5df6bbaa15559e5f481b9edd
minikin <djminikin@gmail.com>
2020-04-12 10:41:28 +0200

    Add AudioSlider

---
+++ lib/src/app/features/audio_player/widgets/audio_slider.dart
@@ -0,0 +1,25 @@
+import 'package:audio_player_flutter/src/services/services.dart' show AudioFile;
+import 'package:flutter/material.dart';
+
+class AudioSlider extends StatelessWidget {
+  final AudioFile audioFile;
+  final AudioPlayerBloc bloc;
+
+  const AudioSlider({
+    @required this.audioFile,
+    @required this.bloc,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Slider(
+      value: bloc.trackPosition,
+      min: 0,
+      max: audioFile.duration.toDouble(),
+      activeColor: Colors.black,
+      onChanged: (value) => bloc.seekTo(value.toInt()),
+      onChangeEnd: (value) => bloc.resume(),
+    );
+  }
+}
