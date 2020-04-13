2c2b0919d6dbffed8723a5f6b0f4a8494d4080b3
minikin <djminikin@gmail.com>
2020-04-13 13:25:36 +0200

    Add player_event

--- lib/src/app/features/audio_player/widgets/audio_skip_button.dart
+++ lib/src/app/features/audio_player/widgets/audio_skip_button.dart
@@ -1,3 +1,4 @@
+import 'package:audio_player_flutter/src/app/features/audio_player/models/audio_skip_button_type.dart';
 import 'package:flutter/material.dart';
 
 class AudioSkipButton<S> extends StatelessWidget {
